c0148c644794b7e5827c6a11a645a2815565c5ff
Moein Akbarof <moein.akbarof@trivago.com>
2015-08-11 15:40:36 +0200

    Adding a test vhost

--- iris/recipes/default.rb
+++ iris/recipes/default.rb
@@ -1,3 +1,10 @@
-execute 'apache_configtest' do
+execute 'edit_hosts' do
   command 'echo "127.0.0.1 tsp-apd.tspdev" >> /etc/hosts'
 end
+
+cookbook_file "/etc/apache2/sites-available/010-crawler.conf" do
+  owner "root"
+  group "root"
+  mode "0644"
+end 
+
