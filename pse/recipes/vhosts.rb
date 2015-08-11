8ab65fdb68b150732e2374951d62097538617e88
Moein Akbarof <moein.akbarof@trivago.com>
2015-08-11 16:03:06 +0200

    Fixed vhosts recipe

--- pse/recipes/vhosts.rb
+++ pse/recipes/vhosts.rb
@@ -10,7 +10,10 @@ cookbook_file "/etc/apache2/sites-available/010-apd.conf" do
   mode "0644"
 end
 
-execute 'symlink_vhosts' do
+execute 'symlink_apdr' do
   command 'ln -s /etc/apache2/sites-available/010-apd.conf /etc/apache2/sites-enabled/010-apd.conf'
+end
+
+execute 'symlink_crawler' do
   command 'ln -s /etc/apache2/sites-available/010-crawler.conf /etc/apache2/sites-enabled/010-crawler.conf'
 end
