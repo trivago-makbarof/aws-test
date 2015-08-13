b7d3c2710a74acd095f7e33b1a9505b8976209bc
Moein Akbarof <moein.akbarof@trivago.com>
2015-08-13 14:50:44 +0200

    Another try

--- crawler/recipes/deploy_test.rb
+++ crawler/recipes/deploy_test.rb
@@ -1,7 +1,9 @@
 include_recipe "pse"
 
+Chef::Log.info %Q(Deploying application "#{node['deploy']['root_path']}")
+
 pse_deploy 'test' do 
-  user node['deployer']['user']
+  user node['deploy']['root_path']
   group node['deployer']['group']
   deploy_root node['deploy']['root_path']
 end 
